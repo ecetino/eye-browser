a6a2f6f84c665eb1ebd84192cc27c9a56598353b
Edgar Cetino <cetino-e@hotmail.com>
2020-12-30 11:35:59 -0800

    initial toggle targets functionality

--- client/src/components/Targets.jsx
+++ client/src/components/Targets.jsx
@@ -10,6 +10,13 @@ class Targets extends React.Component {
       t4: true,
       t5: true
     }
+    this.toggleTarget = this.toggleTarget.bind(this);
+  }
+
+  toggleTarget (target) {
+    this.setState({
+      [target]: false
+    })
   }
 
   render () {
@@ -17,12 +24,20 @@ class Targets extends React.Component {
       return (
         <div>
           {this.state.t1 &&
-            <button style={{'backgroundColor': 'white', 'border': 'none'}} ><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          <button name='t1' style={{'backgroundColor': 'white', 'border': 'none'}}  onClick={() => {this.toggleTarget('t1')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
-          <button style={{'backgroundColor': 'white', 'border': 'none'}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
-          <button style={{'backgroundColor': 'white', 'border': 'none'}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
-          <button style={{'backgroundColor': 'white', 'border': 'none'}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
-          <button style={{'backgroundColor': 'white', 'border': 'none'}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {this.state.t2 &&
+          <button name='t2' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t2')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          }
+          {this.state.t3 &&
+          <button name='t3' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t3')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          }
+          {this.state.t4 &&
+          <button name='t4' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t4')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          }
+          {this.state.t5 &&
+          <button name='t5' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t5')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          }
         </div>
       )
     } else {

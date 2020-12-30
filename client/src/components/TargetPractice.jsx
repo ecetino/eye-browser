1117593560f3f34d937f91cdaf957d44fdae3ed4
Edgar Cetino <cetino-e@hotmail.com>
2020-12-30 12:31:51 -0800

    target practice functionality complete

--- client/src/components/TargetPractice.jsx
+++ client/src/components/TargetPractice.jsx
@@ -5,9 +5,19 @@ class TargetPractice extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ready: false
+      ready: false,
+      completed: false,
+      t1: true,
+      t2: true,
+      t3: true,
+      t4: true,
+      t5: true,
+      targetsChecked: 0
     }
     this.setReady = this.setReady.bind(this);
+    this.setCompleted = this.setCompleted.bind(this);
+    this.reset = this.reset.bind(this);
+    this.toggleTarget = this.toggleTarget.bind(this);
   }
 
   setReady () {
@@ -16,13 +26,49 @@ class TargetPractice extends React.Component {
     })
   }
 
+  setCompleted () {
+    this.setState({
+      completed: !this.state.completed
+    })
+  }
+
+  reset () {
+    this.setState({
+      t1: true,
+      t2: true,
+      t3: true,
+      t4: true,
+      t5: true,
+      targetsChecked: 0,
+      completed: false
+    })
+  }
+
+  toggleTarget (target) {
+    this.setState({
+    [target]: false,
+    targetsChecked: this.state.targetsChecked + 1
+    })
+    if (this.state.targetsChecked === 4) {
+      this.setCompleted();
+    }
+  }
+
   render () {
     return (
       <div>
         <button onClick={() => {this.props.setPage('Home')}}>Home</button>
         <div>Please click ready to begin target practice</div>
+        {!this.state.ready &&
         <button onClick={this.setReady}>Ready</button>
-        <Targets ready={this.state.ready}></Targets>
+        }
+        {this.state.completed &&
+        <div>
+          <h2>Nice job! Completed in y seconds</h2>
+          <button onClick={this.reset}>Try again?</button>
+        </div>
+        }
+        <Targets ready={this.state.ready} reset={this.reset} t1={this.state.t1} t2={this.state.t2} t3={this.state.t3} t4={this.state.t4} t5={this.state.t5} targetsChecked={this.state.targetsChecked} toggleTarget={this.toggleTarget} completed={this.state.completed}></Targets>
       </div>
     )
   }
--- client/src/components/Targets.jsx
+++ client/src/components/Targets.jsx
@@ -1,51 +1,36 @@
 import React from 'react';
 
-class Targets extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      t1: true,
-      t2: true,
-      t3: true,
-      t4: true,
-      t5: true
-    }
-    this.toggleTarget = this.toggleTarget.bind(this);
-  }
-
-  toggleTarget (target) {
-    this.setState({
-      [target]: false
-    })
-  }
-
-  render () {
-    if (this.props.ready) {
-      return (
+const targets = (props) => {
+  if (props.ready) {
+    return (
+      <div>
+        {!props.completed &&
+        <button onClick={props.reset}>restart</button>
+        }
         <div>
-          {this.state.t1 &&
-          <button name='t1' style={{'backgroundColor': 'white', 'border': 'none'}}  onClick={() => {this.toggleTarget('t1')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {props.t1 &&
+          <button name='t1' style={{'backgroundColor': 'white', 'border': 'none'}}  onClick={() => {props.toggleTarget('t1')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
-          {this.state.t2 &&
-          <button name='t2' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t2')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {props.t2 &&
+          <button name='t2' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {props.toggleTarget('t2')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
-          {this.state.t3 &&
-          <button name='t3' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t3')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {props.t3 &&
+          <button name='t3' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {props.toggleTarget('t3')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
-          {this.state.t4 &&
-          <button name='t4' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t4')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {props.t4 &&
+          <button name='t4' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {props.toggleTarget('t4')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
-          {this.state.t5 &&
-          <button name='t5' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {this.toggleTarget('t5')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
+          {props.t5 &&
+          <button name='t5' style={{'backgroundColor': 'white', 'border': 'none'}} onClick={() => {props.toggleTarget('t5')}}><img src='https://lazy-eye-trainer.s3-us-west-1.amazonaws.com/Target.png' style={{'height': '150px'}}/></button>
           }
         </div>
-      )
-    } else {
-      return (
-        <div></div>
-      )
-    }
+      </div>
+    )
+  } else {
+    return (
+      <div></div>
+    )
   }
 }
 
-export default Targets;
+export default targets;

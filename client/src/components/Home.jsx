cac3e33cabbe68a3ad3a93752f255debf0394c6c
Edgar Cetino <cetino-e@hotmail.com>
2021-02-09 21:12:21 -0800

    initial progress on login screen refactor;

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,28 +3,48 @@ import BrowserPractice from './BrowserPractice';
 import KeyboardPractice from './KeyboardPractice';
 import TargetPractice from './TargetPractice';
 import Login from './Login';
-import eye from '../images/eye.png';
+import eye from '../images/4432.svg';
 
 const home = (props) => {
   return (
-    <div className='container-fluid text-center'>
-      <img src={eye} className='img-fluid' style={eyeImageStyle} />
-      <h2 className='display-2' style={welcomeStyle}>Welcome to  <br/> Lazy Eye Trainer</h2>
-      {!props.loggedIn &&
-        <Login testLogin={props.testLogin} loginFailed={props.loginFailed} setLoginFailed={props.setLoginFailed} setLoggedIn={props.setLoggedIn} setUser={props.setUser} user={props.user} password={props.password} confirmPassword={props.confirmPassword} setLoginInfo={props.setLoginInfo} ></Login>
-      }
-      {props.loggedIn &&
-        <div >
-          <h2 style={moduleStyle}>Please select a training module</h2>
-          <div>
-            <button style={moduleButtonStyle} type='button' className='btn btn-primary' onClick={() => { props.setPage('KeyboardPractice') }}>Keyboard Practice</button>
-            <button style={moduleButtonStyle} type='button' className='btn btn-secondary' onClick={() => { props.setPage('TargetPractice') }}>Target Practice</button>
-            <button style={moduleButtonStyle} type='button' className='btn btn-danger' onClick={props.setPage.bind(this, 'BrowserPractice')}>Browser Practice</button>
+    <div style={homeContainer}>
+      <div style={homeImageContainer}>
+        <img src={eye} style={eyeImageStyle} />
+        <h2 className='display-2' style={welcomeStyle}>Welcome to  <br /> Lazy Eye Trainer</h2>
+      </div>
+      <div style={homeLoginContainer}>
+        {!props.loggedIn &&
+          <Login testLogin={props.testLogin} loginFailed={props.loginFailed} setLoginFailed={props.setLoginFailed} setLoggedIn={props.setLoggedIn} setUser={props.setUser} user={props.user} password={props.password} confirmPassword={props.confirmPassword} setLoginInfo={props.setLoginInfo} ></Login>
+        }
+        {props.loggedIn &&
+          <div >
+            <h2 style={moduleStyle}>Please select a training module</h2>
+            <div>
+              <button style={moduleButtonStyle} type='button' className='btn btn-primary' onClick={() => { props.setPage('KeyboardPractice') }}>Keyboard Practice</button>
+              <button style={moduleButtonStyle} type='button' className='btn btn-secondary' onClick={() => { props.setPage('TargetPractice') }}>Target Practice</button>
+              <button style={moduleButtonStyle} type='button' className='btn btn-danger' onClick={props.setPage.bind(this, 'BrowserPractice')}>Browser Practice</button>
+            </div>
           </div>
-        </div>
-      }
+        }
+      </div>
+
     </div>
   )
+}
+
+const homeContainer = {
+  display: 'grid',
+  gridTemplateRows: 'auto',
+  gridTemplateColumns: '2fr 1fr',
+  alignContent: 'center'
+}
+
+const homeImageContainer = {
+  gridColumnStart: 'col-start 0',
+}
+
+const homeLoginContainer = {
+  gridColumnStart: 'col-start 1',
 }
 
 const welcomeStyle = {
@@ -35,9 +55,10 @@ const welcomeStyle = {
 const eyeImageStyle = {
   maxWidth: '80%',
   objectFit: 'cover',
-  width: '500px',
-  maxHeight: '220px',
-  marginTop: '30px'
+  width: '400px',
+  maxHeight: '400px',
+  marginTop: '30px',
+  display: 'inline'
 }
 const moduleStyle = {
   fontFamily: 'Playfair Display',
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg)$/,
+        test: /\.(png|jpg|svg)$/,
         use: {
           loader: 'file-loader',
         },
